fb239b389b1cb1b9cca15c44afb937070bd974e8
Brandon <50503122+BrandonLeach@users.noreply.github.com>
2020-06-05 12:08:39 -0400

    Create vwInterface.java

---
+++ vwInterface.java
@@ -0,0 +1,20 @@
+package Alpha;
+
+import java.util.Scanner;
+
+public interface vwInterface {
+	public default float getFloat(String prompt) {
+		return Float.parseFloat(this.getString(prompt));
+	}
+	public default boolean getBoolean(String prompt) {
+		return (getString(prompt + " (Y is yes)").toUpperCase().contains("Y"));
+	}
+	public default String getString(String prompt) {
+		String response = "";
+		Scanner reader = new Scanner(System.in);
+		System.out.print(prompt + " Press enter when ready. ");
+		response = reader.nextLine();
+		//reader.close();
+		return response;
+	}
+}
